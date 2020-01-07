10bbf8917c735a59edd65524265de10854981955
lh <lh@qq.com>
2020-01-07 09:08:41 +0800

    test3

--- spark-core/src/main/scala/com/atguigu/spark/core/day01/GlomRDD.scala
+++ spark-core/src/main/scala/com/atguigu/spark/core/day01/GlomRDD.scala
@@ -8,6 +8,7 @@ object GlomRDD {
     val conf: SparkConf = new SparkConf().setAppName("FilterRDD").setMaster("local[2]")
     val sc = new SparkContext(conf)
     val rdd1: RDD[Int] = sc.parallelize(Array(10, 60, 70, 50, 65, 35, 25, 12))
+    //glom:把每个分区元素放入一个数组
     val rdd2: RDD[Array[Int]] = rdd1.glom()
 
     rdd2.collect.foreach(x=>println(x.mkString(",")))
